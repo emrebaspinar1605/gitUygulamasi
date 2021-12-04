d15c1b2fe3e6dd9dc65dbbf8a0f7bcbfeaa57b3e
Turku <Turku@DESKTOP-T8QF1UU>
2021-12-04 15:57:32 +0300

    Proje dosyası ekle.

---
+++ gitUygulamasi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
